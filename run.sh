3e833c874c9c2b995ebbdf44d2728d570f63b430
error-sudo <66356720+error-sudo@users.noreply.github.com>
2021-03-22 18:21:36 +0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Ini Script Kedua Dari Script Install"
+sleep 0.1
+echo "Script Khusus Dedi Untuk Melakukan ddos Ke smpn**"
+sleep 1.0
+echo "Script ini khusus untuk Anda Yang Ingin ddos tanpa perlu Meminta bantuan orang"
+sleep 1.0
+echo "ingat ya jika anda setelah menjalankan script ini... anda tidak perlu lagi memasukkan cd pureddos atau apalah itu"
+sleep 1.0
+echo "Made by CYBERDARK"
+sleep 2.0
+cd ddos && cd pureddos && python2 pureddos.py
